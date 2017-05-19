7c0bfec358fa0caaec0b8378994b6146ba9735d6
chilliyo <coxonerica@gmail.com>
2017-05-19 16:18:21 -0500

    Fixed issues in urls.py

--- src/MySite/urls.py
+++ src/MySite/urls.py
@@ -4,10 +4,16 @@ from django.conf.urls import include
 from django.conf.urls.static import static
 from django.conf import settings
 
+from MyHome.views import home, contact
+from MySite.views import about
+
 urlpatterns = [
-    url(r'^$', 'MyHome.views.home', name='home'),
-    url(r'^contact/$', 'MyHome.views.contact', name='contact'),
-    url(r'^about/$', 'MySite.views.about', name='about'),
+    #url(r'^$', 'MyHome.views.home', name='home'),
+    url(r'^$', home, name='home'),
+    #url(r'^contact/$', 'MyHome.views.contact', name='contact'),
+    url(r'^contact/$', contact, name='contact'),
+    #url(r'^about/$', 'MySite.views.about', name='about'),
+    url(r'^about/$', about, name='about'),
     #url(r'^accounts/', include('registration.backends.default.urls')),
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^admin/', admin.site.urls),
